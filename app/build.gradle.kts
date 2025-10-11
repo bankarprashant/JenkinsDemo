34d02a5cf0176a4c786487620b68fccf48f8677f
bankarprashant <bankarprashant17@gmail.com>
2025-10-11 23:05:05 +0530

    Auto-increment versionCode 7 [CI_SKIP]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.jenkinsdemo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
